5c2ead9fdfd1995cb2a37e8495d1046680589015
Henry.Huang <hhglory@outlook.com>
2022-11-16 13:29:20 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="docs/guide/intro">
+                        to="docs/guide/roadmap">
 
                         Rust 学习教程
 
